25a1e612a6016efc35d61e72c72fd5dde77ba6a8
Chandler Mayo <chandler@chandlermayo.com>
2018-10-25 13:38:05 -0700

    Replace keys with placeholder strings.

--- HelloPush/app/src/main/java/com/pubnub/hellopush/MainActivity.java
+++ HelloPush/app/src/main/java/com/pubnub/hellopush/MainActivity.java
@@ -28,8 +28,8 @@ public class MainActivity extends AppCompatActivity {
     // This instance will be used when we need to create connection to PubNub.
     private void initPubnub() {
         PNConfiguration pnConfiguration = new PNConfiguration();
-        pnConfiguration.setPublishKey("pub-c-30a319f8-f814-480e-ba4d-56f29a778e24");
-        pnConfiguration.setSubscribeKey("sub-c-c93ecd58-d7d3-11e8-b7c4-3e16c06ff365");
+        pnConfiguration.setPublishKey("YOUR_PUB_KEY_HERE");
+        pnConfiguration.setSubscribeKey("YOUR_SUB_KEY_HERE");
         pnConfiguration.setSecure(true);
         pubnub = new PubNub(pnConfiguration);
     }
